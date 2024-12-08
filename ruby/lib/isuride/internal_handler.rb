a0683dea8b6fca5b72305e6b18945dd01a56dd11
ohbarye <over.rye@gmail.com>
2024-12-08 14:22:13 +0900

    better matching

--- ruby/lib/isuride/internal_handler.rb
+++ ruby/lib/isuride/internal_handler.rb
@@ -14,7 +14,35 @@ module Isuride
       end
 
       10.times do
-        matched = db.query('SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1').first
+        # matched = db.query('SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1').first
+        # unless matched
+        #   halt 204
+        # end
+
+        chairs = db.query(<<~SQL)
+select chair_id,
+       longitude,
+       latitude
+from chairs
+inner join
+(select cl.chair_id,
+        latitude,
+        longitude
+from chair_locations
+inner join
+(select chair_id,
+        max(created_at) as last_updated_at
+from chair_locations cl
+group by chair_id) cl on chair_locations.created_at = cl.last_updated_at) loc on loc.chair_id = chairs.id
+where is_active = true
+SQL
+
+        matched = chairs.map{|chair|
+          {
+            id: chair.fetch(:chair_id),
+            distance: calculate_distance(ride.fetch(:pickup_latitude), ride.fetch(:pickup_longitude), chair.fetch(:latitude), chair.fetch(:longitude))
+          }
+        }.min_by{_1[:distance]}
         unless matched
           halt 204
         end
