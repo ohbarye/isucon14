04af09bfc74dbd37bb3ac46ce6298f8037485666
ohbarye <over.rye@gmail.com>
2024-12-08 12:24:43 +0900

    SELECT * FROM rides WHERE chair_id = ? ORDER BY created_at DESC LIMIT 1: add index

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -96,6 +96,7 @@ CREATE TABLE rides
   PRIMARY KEY (id)
 )
   COMMENT = 'ライド情報テーブル';
+ALTER TABLE rides ADD INDEX idx_chair_created (chair_id, created_at DESC);
 
 DROP TABLE IF EXISTS ride_statuses;
 CREATE TABLE ride_statuses
