254e8e8ca07672a5fa837b2d3b6040fd449eb63e
ohbarye <over.rye@gmail.com>
2024-12-08 16:44:57 +0900

    fix bug: id should be in

--- ruby/lib/isuride/app_handler.rb
+++ ruby/lib/isuride/app_handler.rb
@@ -426,7 +426,7 @@ module Isuride
           FROM rides
           WHERE created_at = (SELECT max(created_at)
                               FROM rides
-                              WHERE chair_id in ('01JDFHNTV0KGFZ2N2B4CX0RBC5')
+                              WHERE chair_id in (#{chairs.map{"'" + _1[:id] + "'"}.join(", ")})
                               group by chair_id)
         SQL
         rides = rides.group_by { |ride| ride.fetch(:chair_id) }
